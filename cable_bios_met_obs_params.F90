fcff45f81d78d78c3bd7acecca49584fc0d7b372
Anna Ukkola <a.ukkola@unsw.edu.au>
2021-04-13 12:00:07 +1000

    adding prints

--- cable_bios_met_obs_params.F90
+++ cable_bios_met_obs_params.F90
@@ -305,7 +305,7 @@ MODULE cable_bios_met_obs_params
           WG%TempMaxDayPrev = WG%TempMaxDay
           WG%VapPPa1500Prev = WG%VapPPa1500
 
-print *, "prev day VPD 3pm", WG%VapPPa1500Prev
+print *, "prev day VPD 3pm", WG%VapPPa1500Prev(1005)
 
     !**************** MMY *****************
           IF (ktau /= kend .and. CurYear /= YearEnd) THEN
