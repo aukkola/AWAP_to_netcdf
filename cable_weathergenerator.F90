af7227d2d917374c56e6911c5b26ea27b48d9b57
Anna Ukkola <a.ukkola@unsw.edu.au>
2021-04-13 13:38:15 +1000

    adding clearsky sw to test a fix

--- cable_weathergenerator.F90
+++ cable_weathergenerator.F90
@@ -478,7 +478,7 @@ CONTAINS
       !                                (1.35 * (swdown / swdown_clear) - 0.35))
       
       WG%PhiLd = SBoltz * WG%Temp**(4) * (1. - (1.- 0.65*(act_pressure / WG%Temp)**(0.14)) &
-                  * (1.35 * (swdown / max(clearsky_swdown, 0.000001) - 0.35))
+                  * (1.35 * (swdown / max(clearsky_swdown, 0.000001)) - 0.35))
  
       print *, "LWdown after calculation", WG%PhiLd(1005)
 
